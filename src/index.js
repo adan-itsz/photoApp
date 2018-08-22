d47bee7ac4bb141ce16508e0a1844f3f93310206
Adan De Santiago Avalos <adan1995a@gmail.com>
2018-08-22 00:56:37 -0500

    base landing

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import "./componentes/assets/scss/material-kit-react.css?v=1.2.0";
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
